625f5a81af5bf35ab4d0c9a99efae6e9e96247d8
burnettes <burnettes@etsu.edu>
2018-04-23 16:56:44 -0400

    Finished the js neon sign, need to add LA Noire music
    Signed-off-by: burnettes <burnettes@etsu.edu>

--- js/main.js
+++ js/main.js
@@ -9,14 +9,18 @@ $(document).ready(function () {
         css.insertRule('i, #l' + (i + 1) + '{     ' +
             '        font: normal 250px/ 125px \'Nixie One\', Helvetica, Arial;' +
             '        color: #EEDDE0;' +
-            '        text-shadow: 0 0 30px #EEDB13,' +
-            '        0 0 10px rgba(230, 161, 173, 0.41),' +
-            '        0 0 100px #E6A1AD,' +
-            '        0 0 500px #F0D200,' +
-            '        5px 9px 5px rgba(0, 0, 0, 0.5);', css.cssRules.length);
+            '       -webkit-animation: neon 1.5s ease-in-out infinite alternate;\n' +
+            '       -moz-animation: neon0 1.5s ease-in-out infinite alternate;\n' +
+            '        animation: neon 1.5s ease-in-out infinite alternate;' +
+            '        5px 9px 5px rgba(0, 0, 0, 0.5);'
+            , css.cssRules.length);
         css.insertRule('i, #l' + (i + 1) + '.off{     ' +
             '        color: rgba(46, 46, 46, 0.61);\n' +
-            '        text-shadow: 7px 7px 5px rgba(0, 0, 0, 0.5);', css.cssRules.length);
+            '        text-shadow: 7px 7px 5px rgba(0, 0, 0, 0.5);' +
+            '        -webkit-animation-animation: off 1.5s ease-in-out infinite alternate;\n' +
+            '        -moz-animation: off 1.5s ease-in-out infinite alternate;\n' +
+            '         animation: off 1.5s ease-in-out infinite alternate;'
+            , css.cssRules.length);
         chars[i] = '\"#l' + (i + 1) + '\"';
     }
 
@@ -28,37 +32,43 @@ $(document).ready(function () {
         }
         else
             $("#l1").removeClass("off");
+
         if ($("#l2").is(":hover")) {
             flicker(2);
         }
         else
             $("#l2").removeClass("off");
+
         if ($("#l3").is(":hover")) {
             flicker(3);
         }
         else
             $("#l3").removeClass("off");
+
         if ($("#l4").is(":hover")) {
             flicker(4);
         }
         else
             $("#l4").removeClass("off");
+
         if ($("#l5").is(":hover")) {
             flicker(5);
         }
         else
             $("#l5").removeClass("off");
+
         if ($("#l6").is(":hover")) {
             flicker(6);
         }
         else
             $("#l6").removeClass("off");
+
         if ($("#l7").is(":hover")) {
             flicker(7);
         }
         else
             $("#l7").removeClass("off");
-    }, 50);
+    }, 0);
 
     function flicker(n) {
         $("#l" + n).toggleClass("off");
